171d3fe2dd5506f8c6b2b34dc53e842520990c5a
Ryan Richards <65893895+ryan-richards@users.noreply.github.com>
2021-10-25 11:30:36 +0100

    Update email_api.js

--- api/email_api.js
+++ api/email_api.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const email = require('./email');
+var cors = require('cors')
 
 
-app.use(express.urlencoded({extended: true})); 
-app.use(express.json());
+router.use(express.urlencoded({extended: true})); 
+router.use(express.json());
 
-router.post("/", async (req, res, next) => {
-    const name
+router.options('/api/email_api', cors())
+
+router.post("/", cors(), async (req, res, next) => {
     try {
       res.send(await email.sendOrderConfirmation(req.body));
     } catch (err) {
