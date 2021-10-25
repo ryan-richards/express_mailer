5ab230c78c40f2bdf2d8a03e0ad709389bc31bd1
Anna Rowan <annarowan@Annas-MacBook-Pro.local>
2021-10-25 11:27:19 +0100

    cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const emailApi = require('./api/email_api')
+var cors = require('cors')
 
 const app = express();
 const port = 3000 || process.env.PORT;
 
+app.use(cors())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
 
 app.use("/api/email_api", emailApi);
 
