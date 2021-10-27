42a638afc385505087e3a7fbed6c44855bd64af4
Ryan Richards <rynrichards@icloud.com>
2021-10-27 15:21:24 +0100

    require

--- api/email_api.js
+++ api/email_api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const email = require('./email');
+const supabase = require('./supabase');
 var cors = require('cors')
 
 router.use(cors({
