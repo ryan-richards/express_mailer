5d674208c2ba912432eae707ed3e69bce92874ef
Ryan Richards <rynrichards@icloud.com>
2021-10-27 15:41:03 +0100

    supabase anon key env

--- api/supabase.js
+++ api/supabase.js
@@ -1,9 +1,9 @@
 const { createClient } = require('@supabase/supabase-js')
 
 const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_KEY = process.env.SUPABASE_KEY
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 const get_inquires = async () => {
     let { data, error } = await supabase
