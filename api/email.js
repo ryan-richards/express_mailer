1eaaed26b0d72be92ecc9d0ae4a8690ec50e76e0
Ryan Richards <rynrichards@icloud.com>
2021-10-28 17:30:25 +0100

    contact test

--- api/email.js
+++ api/email.js
@@ -5,7 +5,7 @@ const supabase = require('./supabase');
 
 async function getData(params){
   console.log(params)
-  const emailParams = await supabase.get_inquires(params.table);
+  const emailParams = await supabase.get_inquires(params);
   console.log(emailParams[0].recipient);
   await sendNotification(emailParams[0]);
 }
